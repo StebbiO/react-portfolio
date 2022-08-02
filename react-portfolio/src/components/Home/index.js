5eab8ed2c80795720042e056faa213ba35c9a4cf
Stebbi <stefodd@yahoo.com>
2022-08-02 16:58:30 -0500

    added and styled home page

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import './index.scss';
+import LogoTitle from '../../assets/images/logo-s.png';
+
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1> Hi, <br /> I'm
+                <img src={LogoTitle} alt="developer" />
+                tefan
+                <br />
+                web developer
+                </h1>
+                <h2>Full stack developer</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
