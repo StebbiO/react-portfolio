e81147170a609714dea6fb351f75bc2641c9e794
Stebbi <stefodd@yahoo.com>
2022-08-08 16:46:46 -0500

    spinner spinning

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -31,22 +31,22 @@ const About = () => {
             <div className='stage-cube-cont'>
                 <div className='cubespinner'>
                     <div className='face1'>
-                        <FontAwesomeIcon icon={faNode} color="#DD0031" />
+                        <FontAwesomeIcon icon={faNode} color="#3C873A" />
                     </div>
                     <div className='face2'>
-                        <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
                     </div>
                     <div className='face3'>
-                        <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
                     </div>
                     <div className='face4'>
-                        <FontAwesomeIcon icon={faReact} color="#DD0031" />
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
                     </div>
                     <div className='face5'>
-                        <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
                     </div>
                     <div className='face6'>
-                        <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
                     </div>
                 </div>
             </div>
